ab5512d22e5edda9cec0c27d30d91bfaf60fbec6
phitchcock <phitchcock@me.com>
2016-03-14 21:15:47 -0700

    base ios + pod install

---
+++ elected-ios/Pods/Target Support Files/Pods-elected-ios/Pods-elected-ios-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_elected_iosVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_elected_iosVersionString[];
+
---
+++ elected-ios/elected-iosTests/elected_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  elected_iosTests.swift
+//  elected-iosTests
+//
+//  Created by Peter Hitchcock on 3/14/16.
+//  Copyright © 2016 Peter Hitchcock. All rights reserved.
+//
+
+import XCTest
+@testable import elected_ios
+
+class elected_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
