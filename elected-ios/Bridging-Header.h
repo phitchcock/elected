4d3b9e38625de1bffb08b99bcabde05cd2840cce
Peter Hitchcock <phitchcock@me.com>
2016-03-31 09:22:32 -0700

    installed emptydataset

---
+++ elected-ios/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  elected-ios
+//
+//  Created by Peter Hitchcock on 3/31/16.
+//  Copyright © 2016 Peter Hitchcock. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "UIScrollView+EmptyDataSet.h"
+
+#endif /* Bridging_Header_h */
